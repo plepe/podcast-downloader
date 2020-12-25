9302c31459cf5e6fc98a47f480b494dfaa07d73c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-12-25 22:34:29 +0100

    Download webpage, print list to stdout

--- cli.js
+++ cli.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
-console.log('CLI')
+const fetch = require('node-fetch')
+const JSDOM = require('jsdom').JSDOM
+
+fetch('https://scienceblogs.de/astrodicticum-simplex/sternengeschichten/')
+  .then(response => response.text())
+  .then(body => {
+    const dom = new JSDOM(body)
+    const document = dom.window.document
+
+    const list = document.querySelectorAll('.content > ul > li')
+    const result = Array.from(list).map(entry => {
+      const m = entry.textContent.match(/^(.*) Download/)
+      const title = m ? m[1] : entry.textContent
+
+      const links = entry.getElementsByTagName('a')
+      const file = links.length >= 2 ? links[1].href : null
+
+      return { title, file }
+    })
+
+    console.log(JSON.stringify(result, null, '  '))
+  })
