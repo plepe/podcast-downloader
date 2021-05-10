2d713046214e5f9cdb0d0d02f19f1a00b527bfef
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-05-10 21:30:18 +0200

    Iterate list asynchroneously

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const fetch = require('node-fetch')
 const JSDOM = require('jsdom').JSDOM
+const async = require('async')
 
 fetch('https://scienceblogs.de/astrodicticum-simplex/sternengeschichten/')
   .then(response => response.text())
@@ -9,15 +10,14 @@ fetch('https://scienceblogs.de/astrodicticum-simplex/sternengeschichten/')
     const document = dom.window.document
 
     const list = document.querySelectorAll('.content > ul > li')
-    const result = Array.from(list).map(entry => {
+    async.mapLimit(list, 4, (entry, done) => {
       const m = entry.textContent.match(/^(.*) Download/)
       const title = m ? m[1] : entry.textContent
 
       const links = entry.getElementsByTagName('a')
       const file = links.length >= 2 ? links[1].href : null
 
-      return { title, file }
-    })
-
-    console.log(JSON.stringify(result, null, '  '))
+      done(null, { title, file })
+    },
+    (err, result) => console.log(JSON.stringify(result, null, '  ')))
   })
