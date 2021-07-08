ba0f0364a8b4f2bf34d4d174960788101d0d53b7
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-07-08 18:59:37 +0200

    normalize

--- cli.js
+++ cli.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch')
 const JSDOM = require('jsdom').JSDOM
 const async = require('async')
 const fs = require('fs')
+const child_process = require('child_process')
 
 fetch('https://scienceblogs.de/astrodicticum-simplex/sternengeschichten/')
   .then(response => response.text())
@@ -22,14 +23,52 @@ fetch('https://scienceblogs.de/astrodicticum-simplex/sternengeschichten/')
 	const m = file.match(/\/([^/]+)$/)
 	const filename = m[1]
 
-	fetch(file)
-	  .then(response => response.buffer())
-	  .then(body => fs.writeFile('data/' + filename, body,
-	    (err) => done(err, { title, file: 'data/' + filename })
-	  ))
+	fs.stat('data/' + filename,
+	  (err, data) => {
+	    if (!err) {
+	      console.log(filename, 'exists')
+	      return done(null, { title, file: 'data/' + filename })
+	    }
+
+	    fetch(file)
+	      .then(response => response.buffer(),
+	        err => {
+		  console.log('error downloading', filename)
+		})
+	      .then(body => fs.writeFile('/tmp/' + filename, body,
+		(err) => {
+		  if (err) { return done(err) }
+		  normalize('/tmp/' + filename, 'data/' + filename,
+		    (err) => {
+		      console.log('err', err)
+		      done(err, { title, file: 'data/' + filename })
+		    }
+		  )
+		})
+	      )
+	  }
+	)
       } else {
 	done(null, { title })
       }
     },
-    (err, result) => console.log(JSON.stringify(result, null, '  ')))
+    (err, result) => fs.writeFile('playlist.json', JSON.stringify(result, null, '  ')))
   })
+
+function normalize (src, dest, callback) {
+  console.log(callback)
+  fs.stat(src,
+    (err, data) => {
+      if (err) {
+	console.log(src, 'does not exists')
+	return callback(null)
+      }
+
+      child_process.execFile('ffmpeg', [ '-i', src, '-filter:a', 'loudnorm', dest ], {}, (err) => {
+	if (err) { console.error(err) }
+	fs.unlink(src, () => {})
+	callback(err)
+      })
+    }
+  )
+}
