de81efd8b7c50baa0dfb3e416c8b2260710b81aa
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-07-22 11:41:45 +0200

    Split code into classes

--- cli.js
+++ cli.js
@@ -2,8 +2,8 @@
 const fetch = require('node-fetch')
 const JSDOM = require('jsdom').JSDOM
 const async = require('async')
-const fs = require('fs')
-const child_process = require('child_process')
+
+const PodcastEpisode = require('./src/PodcastEpisode')
 
 const def = {
   type: 'html',
@@ -16,8 +16,7 @@ const def = {
 }
 
 const config = {
-  parallelDownloads: 1,
-  parallelNormalizers: 4
+  parallelTasks: 4
 }
 
 function parseListFromPage (callback) {
@@ -29,136 +28,53 @@ function parseListFromPage (callback) {
 
       const list = document.querySelectorAll(def.htmlQuerySelector)
       async.mapLimit(list, 4, (entry, done) => {
-	d = {}
+	const episode = new PodcastEpisode(def, config)
 
-        d.htmlTitle = entry.textContent
-
-	let m = entry.textContent.match(def.htmlTitleRegexp)
-	d.title = (m ? m[1] : entry.textContent).trim()
-
-	m = entry.textContent.match(def.htmlIdRegexp)
-	d.id = m ? m[1].trim() : null
-
-	m = entry.textContent.match(def.htmlNameRegexp)
-	d.name = m ? m[1].trim() : null
-
-	const links = entry.getElementsByTagName('a')
-	d.url = links.length > def.htmlUseLinkNum ? links[def.htmlUseLinkNum].href : null
-
-	done(null, d)
+        episode.parseHtmlTitle(entry, err => done(err, episode))
       },
       callback)
     })
 }
 
-function useUrlAsFile (data, callback) {
-  data.forEach(entry => entry.file = entry.url)
-  callback(null, data)
-}
-
-function generateFilenames (data, callback) {
-  data.forEach(entry => {
-    entry.filename = entry.id + ' - ' + entry.name + '.mp3'
-  })
-
-  callback(null, data)
-}
-
-function downloadFiles (data, callback) {
-  async.eachLimit(data, config.parallelDownloads, downloadFile, err => callback(err, data))
-}
-
-function downloadFile (entry, callback) {
-  if (!entry.filename) {
-    return callback(null)
-  }
-
-  const destFile = 'orig/' + entry.filename
-
-  fs.stat(destFile,
-   (err, data) => {
-     if (!err) {
-       console.error(entry.filename, 'exists')
-       entry.file = destFile
-       entry.downloadFile = destFile
-       return callback(null, entry)
-     }
-
-     console.error("Downloading", entry.filename)
-     fetch(entry.url)
-       .then(response => response.buffer(),
-         err => {
-           console.error('error downloading', entry.filename)
-         })
-       .then(body => fs.writeFile(destFile, body,
-         (err) => {
-           if (err) { return callback(err) }
-
-	   entry.file = destFile
-	   entry.downloadFile = destFile
-           callback(null, entry)
-         })
-       )
+function printResult (list, callback) {
+  async.map(list,
+    (entry, done) => entry.printResult(done),
+    (err, data) => {
+      if (err) { return console.error(err) }
+      console.log(JSON.stringify(data, null, '  '))
+      callback(null)
     }
   )
 }
 
-function normalizeFiles (data, callback) {
-  async.eachLimit(data, config.parallelNormalizers, normalizeFile, err => callback(err, data))
+function select (data, callback) {
+  data = data.slice(0, 2)
+  callback(null, data)
 }
 
-function normalizeFile (entry, callback) {
-  const destFile = 'data/' + entry.filename
+function debugAll (list, callback) {
+  console.error(list)
+  callback(null, list)
+}
 
-  async.parallel(
-    {
-      srcStat: done => fs.stat(entry.downloadFile, (err, result) => {
-	done(null, result)
-      }),
-      destStat: done => fs.stat(destFile, (err, result) => {
-	if (err) {
-	 return done(null)
-	}
-
-	done(err, result)
-      })
-    },
-    (err, { srcStat, destStat }) => {
-      if (err) {
-	console.error(err)
-        return callback(null)
-      }
-
-      if (!srcStat || destStat) {
-        entry.normalizedFile = destFile
-        entry.file = destFile
-	return callback(null)
-      }
-
-      console.error("Normalizing", entry.filename)
-      child_process.execFile('ffmpeg', [ '-i', entry.downloadFile, '-filter:a', 'loudnorm', '-y', destFile ], {}, (err) => {
-        if (err) { console.error(err) }
-        entry.file = destFile
-        entry.normalizedFile = destFile
-
-        callback()
-      })
-    }
+function processAll (list, callback) {
+  async.eachLimit(
+    list,
+    config.parallelTasks,
+    (entry, done) => entry.process(done),
+    err => callback(err, list)
   )
-}
-
-function printResult(data, callback) {
-  console.log(JSON.stringify(data, null, '  '))
-  callback(null)
 }
 
 async.waterfall(
   [
     parseListFromPage,
-    useUrlAsFile,
-    generateFilenames,
-    downloadFiles,
-    normalizeFiles,
+    select,
+    processAll,
+    debugAll,
+//    generateFilenames,
+//    downloadFiles,
+//    normalizeFiles,
     printResult
   ],
   err => {
---
+++ src/PodcastEpisode.js
@@ -0,0 +1,138 @@
+const fetch = require('node-fetch')
+const async = require('async')
+const fs = require('fs')
+const child_process = require('child_process')
+
+module.exports = class PodcastEpisode {
+  constructor (def, config) {
+    this.def = def
+    this.config = config
+  }
+
+  parseHtmlTitle (dom, callback) {
+    this.htmlTitle = dom.textContent
+
+    let m = dom.textContent.match(this.def.htmlTitleRegexp)
+    this.title = (m ? m[1] : dom.textContent).trim()
+
+    m = dom.textContent.match(this.def.htmlIdRegexp)
+    this.id = m ? m[1].trim() : null
+
+    m = dom.textContent.match(this.def.htmlNameRegexp)
+    this.name = m ? m[1].trim() : null
+
+    const links = dom.getElementsByTagName('a')
+    this.url = links.length > this.def.htmlUseLinkNum ? links[this.def.htmlUseLinkNum].href : null
+
+    callback()
+  }
+
+  useUrlAsFile (callback) {
+    this.file = this.url
+    callback()
+  }
+
+  generateFilename (callback) {
+    this.filename = this.id + ' - ' + this.name + '.mp3'
+    callback()
+  }
+
+  downloadFile (callback) {
+    if (!this.filename) {
+      console.error(this.id, 'no filename -> skip')
+      return callback(null)
+    }
+
+    const destFile = 'orig/' + this.filename
+
+    fs.stat(destFile,
+     (err, data) => {
+       if (!err) {
+         console.error(this.id, this.filename, 'exists')
+         this.file = destFile
+         this.downloadFile = destFile
+         return callback(null)
+       }
+
+       console.error(this.id, "Downloading", this.filename)
+
+       fetch(this.url)
+         .then(response => response.buffer(),
+           err => {
+             console.error('error downloading', this.filename)
+           })
+         .then(body => fs.writeFile(destFile, body,
+           (err) => {
+             if (err) { return callback(err) }
+
+             this.file = destFile
+             this.downloadFile = destFile
+             callback(null)
+           })
+         )
+      }
+    )
+  }
+
+  normalizeFile (callback) {
+    const destFile = 'data/' + this.filename
+
+    async.parallel(
+      {
+        srcStat: done => fs.stat(this.downloadFile, (err, result) => {
+          done(null, result)
+        }),
+        destStat: done => fs.stat(destFile, (err, result) => {
+          if (err) {
+           return done(null)
+          }
+
+          done(err, result)
+        })
+      },
+      (err, { srcStat, destStat }) => {
+        if (err) {
+          console.error(err)
+          return callback(null)
+        }
+
+        if (!srcStat || destStat) {
+          this.normalizedFile = destFile
+          this.file = destFile
+          return callback(null)
+        }
+
+        console.error("Normalizing", this.filename)
+        child_process.execFile('ffmpeg', [ '-i', this.downloadFile, '-filter:a', 'loudnorm', '-y', destFile ], {}, (err) => {
+          if (err) { console.error(err) }
+          this.file = destFile
+          this.normalizedFile = destFile
+
+          callback()
+        })
+      }
+    )
+  }
+
+  printResult (callback) {
+    const data = {
+      file: this.file,
+      title: this.title
+    }
+
+    callback(null, data)
+  }
+
+  process (callback) {
+    //this.useUrlAsFile(callback)
+
+    async.waterfall(
+      [
+        done => this.generateFilename(done),
+        done => this.downloadFile(done),
+        done => this.normalizeFile(done)
+      ],
+      callback
+    )
+  }
+}
