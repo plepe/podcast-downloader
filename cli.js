d4365a701c678c5347e0cef048f74aa6b9d0b168
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-07-23 11:15:20 +0200

    Use argparse for CLI parameter handling; choose profile

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const fs = require('fs')
+const ArgumentParser = require('argparse').ArgumentParser
 
 const Podcast = require('./src/Podcast')
 
@@ -7,10 +8,20 @@ const config = {
   parallelTasks: 4
 }
 
-const profile = 'default'
+const parser = new ArgumentParser({
+  add_help: true,
+  description: 'Downloads all episodes of a podcast and optionally normalizes them. Also creates a JSON playlist.'
+})
+
+parser.add_argument('--profile', '-p', {
+  help: 'Which profile to use (default: "default").',
+  default: 'default'
+})
+
+const args = parser.parse_args()
 
 fs.readFile(
-  'profiles/' + profile + '.json',
+  'profiles/' + args.profile + '.json',
   (err, result) => {
     if (err) {
       console.error(err.toString())
