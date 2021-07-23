de540345ed5b7df8d96ac72dd7fb06a3733f26f6
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-07-23 08:33:30 +0200

    Create sternengeschichten as profile

--- cli.js
+++ cli.js
@@ -1,20 +1,25 @@
 #!/usr/bin/env node
+const fs = require('fs')
 
 const Podcast = require('./src/Podcast')
-
-const def = {
-  type: 'html',
-  url: 'https://scienceblogs.de/astrodicticum-simplex/sternengeschichten/',
-  htmlQuerySelector: '.content > ul > li',
-  htmlIdRegexp: /^Folge ([0-9]*)/,
-  htmlNameRegexp: /^Folge (?:[0-9]*)\s*:?\s*([^\[]+)\s*(Download)/,
-  htmlTitleRegexp: /^(Folge (?:[0-9]*)\s*:?\s*([^\[]+))\s*(Download)/,
-  htmlUseLinkNum: 1
-}
 
 const config = {
   parallelTasks: 4
 }
 
-const podcast = new Podcast(def, config)
-podcast.process()
+const profile = 'sternengeschichten'
+
+fs.readFile(
+  'profiles/' + profile + '.json',
+  (err, result) => {
+    if (err) {
+      console.error(err.toString())
+      process.exit(1)
+    }
+
+    const def = JSON.parse(result)
+
+    const podcast = new Podcast(def, config)
+    podcast.process()
+  }
+)
